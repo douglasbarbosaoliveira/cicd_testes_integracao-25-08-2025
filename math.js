a2e4176f304af70f23218ae0d87c5eadba206c52
Douglas Barbosa de Oliveira <dboestudos@gmail.com>
2025-08-26 00:03:09 +0000

    Programa finalizado

---
+++ math.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+    return  a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a + b;
+}
+
+const divide = (a, b) => {
+    if (b == 0) {
+        return "Erro. A divisão por zero não é permitida."
+    }
+    return a / b;
+}
+
+module.exports = {add, subtract, multiply, divide};
